8b4018f26419fc0ca82b3e70b51c76f1317c21f4
Don-Cod <masyome@gmail.com>
2024-02-21 11:31:40 -0800

    Unittests_and_integration_tests

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+""" Unittest_module """
+import unittest
+from client import GithubOrgClient
+from unittest.mock import patch, PropertyMock
+from parameterized import parameterized, parameterized_class
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Class_testing GithubOrgClient """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_json):
+        """ Test method thatreturns correct output """
+        endpoint = 'https://api.github.com/orgs/{}'.format(org_name)
+        spec = GithubOrgClient(org_name)
+        spec.org()
+        mock_json.assert_called_once_with(endpoint)
+
+    @parameterized.expand([
+        ("random-url", {'repos_url': 'http://some_url.com'})
+    ])
+    def test_public_repos_url(self, name, result):
+        """ Test method_returns correct output """
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get('repos_url'))
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """Test list of repos."""
+        json_payload = [{"name": "Google"}, {"name": "Yahoo"}]
+        org = "mock"
+        mock_json.return_value = json_payload
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as repos_url:
+            repos_url.return_value = 'hello/world'
+            response = GithubOrgClient(org).public_repos()
+            check = [i["name"] for i in json_payload]
+            self.assertEqual(response, check)
+            mock_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ unit-test_GithubOrgClient.has_license """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ Class for Integration test_fixtures """
+
+    @classmethod
+    def setUpClass(cls):
+        """A class method called before tests"""
+
+        config = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **config)
+
+        cls.mock = cls.get_patcher.start()
+
+    def test_public_repos(self):
+        """ Integration test"""
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.org, self.org_payload)
+        self.assertEqual(test_class.repos_payload, self.repos_payload)
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.mock.assert_called()
+
+    def test_public_repos_with_license(self):
+        """ Integration test> public repositories with License """
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.assertEqual(test_class.public_repos(
+            "apache-2.0"), self.apache2_repos)
+        self.mock.assert_called()
+
+    @classmethod
+    def tearDownClass(cls):
+        """A class method called after individual class have run"""
+        cls.get_patcher.stop()
